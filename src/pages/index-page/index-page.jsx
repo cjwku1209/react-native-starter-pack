5862e12871bf6175a1a5729ecd3d47b57f599b65
Calvin Ku <calvinku@gapp.nthu.edu.tw>
2024-03-01 15:02:04 +0800

    Safe area view

--- src/pages/index-page/index-page.jsx
+++ src/pages/index-page/index-page.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Text, View } from "react-native";
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { connect } from "react-redux";
 
 class Component extends React.Component {
 
-	render() {
-		return(
-			<View>
-				<Text style={{textAlign: "center", paddingTop: "50%"}}>
-					Hello World!
-				</Text>
-			</View>
-		)
-	}
+  render() {
+    return (
+      <SafeAreaView>
+        <View>
+          <Text style={{ textAlign: "center"}}>
+            Hello World!
+          </Text>
+        </View>
+      </SafeAreaView>
+
+    )
+  }
 }
 
 const Redux = connect(store => ({}))(Component);
